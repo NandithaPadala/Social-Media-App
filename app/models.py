edeee94cd39f6ebe19e7778feef4ee6e0e53dea1
NandithaPadala <padalanandihta@gmail.com>
2025-01-26 21:29:52 +0530

    sigin and logout pages added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to = 'profile_images', default='book-icon.png')
+    location = models.CharField(max_length=50,blank=True)
+
+    def __str__(self):
+        return self.user.username
